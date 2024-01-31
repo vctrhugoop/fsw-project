2a9677de7e5690a494c1b9b101ded20029168019
vctrhugoop <vhdestroy@hotmail.com>
2024-01-30 22:07:59 -0300

    ✨ feat: add recommendedCard comenents

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,17 @@
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { db } from "../_lib/prisma";
+
+import { Key } from "react";
 import { BookingCard } from "../_components/bookingCard";
 import { Header } from "../_components/header";
+import { SectionTitle } from "../_components/sectionTitle";
+import { RecomendedCard } from "./_components/RecomendedCard";
 import { Search } from "./_components/Search";
 
-export default function Home() {
+export default async function Home() {
+  const barbershops = await db.barbershop.findMany({});
+
   return (
     <div>
       <Header />
@@ -21,6 +28,14 @@ export default function Home() {
 
       <Search />
       <BookingCard />
+      <div className="space-y-3 px-6">
+        <SectionTitle title="recomendados" />
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop: { id: Key | null | undefined }) => (
+            <RecomendedCard key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
