ae6f0b76324b3685017ea9c0e0d3eff3926d0fcd
vctrhugoop <vhdestroy@hotmail.com>
2024-02-02 18:29:28 -0300

    ✨ feat: add next auth setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+
+import { db } from "@/app/_lib/prisma";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
