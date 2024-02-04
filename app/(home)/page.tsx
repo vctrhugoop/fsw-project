85afc2205848b79ce63a7f88766f70854997bde1
vctrhugoop <vhdestroy@hotmail.com>
2024-02-04 19:02:54 -0300

    ✨ feat: add name user auth

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,14 +2,17 @@ import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { db } from "../_lib/prisma";
 
+import { getServerSession } from "next-auth";
 import { BookingCard } from "../_components/booking-card";
 import { Header } from "../_components/header/header";
 import { Search } from "../_components/search";
 import { SectionTitle } from "../_components/section-title";
 import { ScrollArea, ScrollBar } from "../_components/ui/scroll-area";
+import { authOptions } from "../utils/authOpitions";
 import { BarbershopItem } from "./_components/barbershop-item";
 
 export default async function Home() {
+  const session = await getServerSession(authOptions);
   const barbershops = await db.barbershop.findMany({});
 
   return (
@@ -17,7 +20,9 @@ export default async function Home() {
       <Header />
       <div className="px-5 py-6">
         <h2 className="text-xl">
-          Olá, <strong>Victor</strong>
+          {session?.user
+            ? `Olá, ${session.user.name?.split(" ")[0]}!`
+            : "Olá! Vamos agendar um corte hoje?"}
         </h2>
         <span className="text-sm capitalize">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
